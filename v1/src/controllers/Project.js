840afe70df5b56c89164f08bd7b0d7d3a26db781
eminuzgur <eminuzgur@gmail.com>
2022-07-20 19:24:42 +0300

    joi-validation

--- v1/src/controllers/Project.js
+++ v1/src/controllers/Project.js
@@ -1,6 +1,7 @@
 const httpstatus = require('http-status');
 const { insert, list } = require('../services/Project');
 
+
 const index = (req, res) => {
     list().then((response) => {
         res.status(httpstatus.OK).send(response)
@@ -13,10 +14,9 @@ const create = (req, res) => {
     insert(req.body).then((response) => {
         res.status(httpstatus.CREATED).send(response)
     })
-        .catch((e) => {
-            res.status(httpstatus.INTERNAL_SERVER_ERROR).send(e)
-        });
-
+    .catch((e) => {
+        res.status(httpstatus.INTERNAL_SERVER_ERROR).send(e)
+    });
 };
 
 module.exports = {
--- v1/src/routes/Project.js
+++ v1/src/routes/Project.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const {index,create}=require('../controllers/Project');
+const validate=require('../middlewares/validate')
+const schemas=require('../validations/Project')
 
 const router = express.Router();
 
 router.get('/',index);
-router.post('/',create);
+router
+.route('/')
+.post(validate(schemas.createValidation),create);
 
 module.exports = router;
