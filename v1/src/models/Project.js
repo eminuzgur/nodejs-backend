de6b257c8559048f1df2ea9d4ec16073fae02026
eminuzgur <eminuzgur@gmail.com>
2022-07-20 22:17:58 +0300

    winston logger

--- v1/src/models/Project.js
+++ v1/src/models/Project.js
@@ -1,8 +1,17 @@
 const Mongoose = require('mongoose');
+const logger=require('../scripts/logger/Project')
 
 const ProjectSchema = new Mongoose.Schema({
     name: String,
 },
-    { versionKey: false, timestamps: true });
+    { versionKey: false, timestamps: true }
+);
+
+ProjectSchema.post('save',(doc)=>{
+    logger.log({
+        level:"info",
+        message:doc
+    })
+});
 
 module.exports = Mongoose.model('project', ProjectSchema);
--- v1/src/validations/Project.js
+++ v1/src/validations/Project.js
@@ -1,7 +1,9 @@
 const joi=require('joi');
 
 const createValidation=joi.object({
-    name:joi.string().required().min(5),
+    name:joi.string()
+    .required()
+    .min(5)
 });
 
 module.exports={
