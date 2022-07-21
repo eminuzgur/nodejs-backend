5face3a5ddf204d79c3c306fea024aff203c5c12
eminuzgur <eminuzgur@gmail.com>
2022-07-21 23:47:16 +0300

    password hashing

--- v1/src/controllers/Users.js
+++ v1/src/controllers/Users.js
@@ -1,6 +1,6 @@
 const httpstatus = require('http-status');
 const { insert, list } = require('../services/User');
-
+const {passwordToHash}=require('../scripts/utils/helper')
 
 const index = (req, res) => {
     list().then((response) => {
@@ -11,6 +11,11 @@ const index = (req, res) => {
 };
 
 const create = (req, res) => {
+    const cryptedPassword=passwordToHash(req.body.password);
+    
+    console.log(req.body.password, cryptedPassword);
+
+    return false;
     insert(req.body).then((response) => {
         res.status(httpstatus.CREATED).send(response)
     })
--- v1/src/validations/User.js
+++ v1/src/validations/User.js
@@ -3,7 +3,7 @@ const joi=require('joi');
 const createValidation=joi.object({
     full_name:joi.string().required().min(3),
     password:joi.string().required().min(8),
-    email:joi.string().required().min(8),
+    email:joi.string().email().required().min(8),
 });
 
 module.exports={
